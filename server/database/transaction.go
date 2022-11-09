34a89cf9a91adf21fde33036dc22326e63110eef
Stefan Jagdmann <sjagdmann10@googlemail.com>
2022-11-09 15:09:13 +0100

    Added docu for transaction manager

--- server/database/transaction.go
+++ server/database/transaction.go
@@ -6,8 +6,10 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+// A function with a transaction
 type TxFn func(ctx context.Context) error
 
+// Handles transactions through the application layers
 type TransactionManger interface {
 	WithTransaction(context.Context, TxFn) error
 }
@@ -22,6 +24,8 @@ type tmImpl struct {
 
 var _ TransactionManger = &tmImpl{}
 
+// WithTransaction takes the app context and performs a transaction for fn. If fn returns an error or panics
+// a rollback of the whole transaction is perfomed, otherwise the transaction will be commited.
 func (tm *tmImpl) WithTransaction(ctx context.Context, fn TxFn) (err error) {
 	tx, ok := ctx.Value(TransaktionCtxKey).(*sqlx.Tx)
 	if !ok {
@@ -43,6 +47,7 @@ func (tm *tmImpl) WithTransaction(ctx context.Context, fn TxFn) (err error) {
 	return
 }
 
+// MustHaveTx returns the transaction from ctx or panics if no transaction is available.
 func MustHaveTx(ctx context.Context) (tx *sqlx.Tx) {
 	tx, ok := ctx.Value(TransaktionCtxKey).(*sqlx.Tx)
 	if !ok {
